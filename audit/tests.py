b2328822deb48f6ea802922d9b0322e5f4c2d181
Sulthan Afif Althaf <sulthan.afif@ui.ac.id>
2023-03-05 21:43:10 +0700

    [RED] Menyusun unit test AuditCategory

---
+++ audit/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.apps import apps
+
+from .apps import AuditConfig
+from .models import AuditCategory
+
+# Create your tests here.
+class AuditAppTestCase(TestCase):
+    def test_apps(self):
+        self.assertEqual(AuditConfig.name, 'audit')
+        self.assertEqual(apps.get_app_config('audit').name, 'audit')
+
+class AuditCategoryModelTestCase(TestCase):
+    def setUp(self):
+        self.label = "Kredit"
+
+        self.obj = AuditCategory.objects.create(label = self.label)
+
+    def test_create_audit_category(self):
+        assert isinstance(self.obj, AuditCategory)
+    
+    def test_field_category(self):
+        assert self.label == self.obj.label
