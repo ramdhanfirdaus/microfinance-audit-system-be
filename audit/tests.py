330d30e38ed86b50fe866784aa30a04523edd224
Muhammad Razif Firmansyah <muhammad.razif01@ui.ac.id>
2023-03-06 22:42:39 +0700

    [RED] Membuat unit test untuk AuditType

--- audit/tests.py
+++ audit/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.apps import apps
 
 from .apps import AuditConfig
-from .models import AuditCategory
+from .models import AuditCategory, AuditType
 
 # Create your tests here.
 class AuditAppTestCase(TestCase):
@@ -20,4 +20,15 @@ class AuditCategoryModelTestCase(TestCase):
         assert isinstance(self.obj, AuditCategory)
     
     def test_field_category(self):
+        assert self.label == self.obj.label
+
+class AuditTypeModelTestCase(TestCase):
+    def setUp(self):
+        self.label = "General"
+        self.obj = AuditType.objects.create(label = self.label)
+
+    def test_create_audit_type(self):
+        assert isinstance(self.obj, AuditType)
+    
+    def test_field_type(self):
         assert self.label == self.obj.label
