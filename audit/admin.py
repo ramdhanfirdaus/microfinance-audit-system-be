4c981537dc75d2150b170a7da2c8df5d270b7112
Muhammad Razif Firmansyah <muhammad.razif01@ui.ac.id>
2023-03-07 08:17:17 +0700

    [GREEN] Membuat implementasi AuditType model

--- audit/admin.py
+++ audit/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from.models import AuditCategory
+from.models import AuditType
 
 # Register your models here.
 admin.site.register(AuditCategory)
+admin.site.register(AuditType)
--- audit/models.py
+++ audit/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class AuditCategory(models.Model):
     label = models.CharField(max_length=15)
+
+class AuditType(models.Model):
+    label = models.CharField(max_length=15)
