a0afa695407876b8719a95bd0ed3cde291c5cb24
Muhammad Razif Firmansyah <muhammad.razif01@ui.ac.id>
2023-03-12 08:35:34 +0700

    Menambahkan file untuk scan SonarQube branch Start Audit

--- microfinance_audit_system_be/settings.py
+++ microfinance_audit_system_be/settings.py
@@ -89,12 +89,7 @@ DATABASES = {
        }
    }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 
 
 # Password validation
