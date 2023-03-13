74511a9cf68106b49ce5c07bd8d72e32781fb544
Muhammad Razif Firmansyah <muhammad.razif01@ui.ac.id>
2023-03-13 17:36:24 +0700

    [GREEN] Membuat implementasi model AuditSession

--- audit/models.py
+++ audit/models.py
@@ -8,3 +8,10 @@ class AuditCategory(models.Model):
 class AuditType(models.Model):
     id = models.AutoField(primary_key=True)
     label = models.CharField(max_length=15)
+
+class AuditSession(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.OneToOneField(
+        AuditType,
+        on_delete=models.CASCADE,
+    )
