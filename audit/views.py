72cf31a2a34da76798350b0307dcfc9f42a1d5d6
Sulthan Afif Althaf <sulthan.afif@ui.ac.id>
2023-03-15 14:01:21 +0700

    [GREEN] mengimplementasi API get audit_categories

--- audit/views.py
+++ audit/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+from .models import *
+from .serializer import *
+
+@api_view(['GET'])
+def get_audit_categories(request, id):
+    print(id)
+
+    try :
+        audit_categories = AuditCategory.objects.filter(audit_type = int(id))
+
+        if len(audit_categories) == 0 :
+            raise Exception
+        
+    except Exception :
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = AuditCategorySerializer(audit_categories, many=True)
+    return Response(serializer.data)
+
+
