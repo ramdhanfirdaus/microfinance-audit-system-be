476afcc0af419bb9c972877dbd121f01d574d7d1
Muhammad Razif Firmansyah <muhammad.razif01@ui.ac.id>
2023-03-19 18:37:45 +0700

    [GREEN] Impelemntasi CreateNewAuditSession

--- audit/urls.py
+++ audit/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_all_audit_types
+from .views import create_new_audit_session, get_all_audit_types
 
 urlpatterns = [
     path('get-all-audit-types/', get_all_audit_types),
+    path('create-new-audit-session/', create_new_audit_session),
 ]
--- audit/views.py
+++ audit/views.py
@@ -19,3 +19,10 @@ def get_all_audit_types(request):
     types = AuditType.objects.all()
     serializer = AuditTypeSerializer(types, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_new_audit_session(request, id):
+    types = AuditType.objects.get(id = int(id))
+    serializer = AuditSessionSerializer(type=types)
+    return JsonResponse(serializer.data, status=201)
