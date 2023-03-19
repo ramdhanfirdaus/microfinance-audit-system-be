efb0ff9aaa622eaba04ef5784c2ca2b61280423b
Muhammad Razif Firmansyah <muhammad.razif01@ui.ac.id>
2023-03-19 13:30:47 +0700

    [GREEN] Impelement views dan endpoint untuk create AuditSession

--- audit/urls.py
+++ audit/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_audit_categories
+from .views import get_audit_categories, create_audit_session
 
 urlpatterns = [
-    path('audit-categories/<str:id>', get_audit_categories, name="audit category")
+    path('audit-categories/<str:id>', get_audit_categories, name="audit category"),
+    path('create-session/<str:id>', create_audit_session, name='create_audit_session'),
 ]
--- audit/views.py
+++ audit/views.py
@@ -4,8 +4,12 @@ from rest_framework import status
 
 from django.core.exceptions import ObjectDoesNotExist
 
-from .models import AuditCategory
-from .serializer import AuditCategorySerializer
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from .models import AuditCategory, AuditSession, AuditType
+from .serializer import AuditCategorySerializer, AuditSession, AuditSessionSerializer
 
 @api_view(['GET'])
 def get_audit_categories(request, id):
@@ -23,4 +27,20 @@ def get_audit_categories(request, id):
     serializer = AuditCategorySerializer(audit_categories, many=True)
     return Response(serializer.data)
 
-
+@api_view(['POST'])
+def create_audit_session(request, id):
+    # making sure that type exist
+    try :
+        types = AuditType.objects.filter(id = int(id))
+        if len(types) == 0 :
+            raise ObjectDoesNotExist        
+    except ObjectDoesNotExist :
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    # create session
+    data = JSONParser().parse(request)
+    serializer = AuditSessionSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
