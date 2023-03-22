b0262abd9b40bb2b965550d608e1036cae3df27e
Sulthan Afif Althaf <sulthan.afif@ui.ac.id>
2023-03-23 01:29:03 +0700

    [GREEN] mengimplementasi post_audit_data_views

--- audit/urls.py
+++ audit/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_audit_categories
+from .views import get_audit_categories, post_audit_data
 
 urlpatterns = [
-    path('audit-categories/<str:id>', get_audit_categories, name="audit category")
+    path('audit-categories/<str:id>', get_audit_categories, name="audit category"),
+    path('upload-data', post_audit_data, name="upload data")
 ]
