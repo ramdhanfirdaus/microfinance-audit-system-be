77f6458fa40223f5915e9371607da617d4ecd0ef
Ramdhan Firdaus Amelia <ramdhan.firdaus@ui.ac.id>
2023-04-05 19:43:05 +0700

    [GREEN] mengimplementasikan fitur dari sub tasks Penyimpanan form komentar dan remark serta Penyimpanan form attachment

--- audit/urls.py
+++ audit/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import get_all_audit_types, create_new_audit_session, get_audit_categories, post_audit_data
+from audit.views_questions import post_audit_question_session
 
 urlpatterns = [
     path('get-all-audit-types/', get_all_audit_types, name="get_audit_types"),
     path('create-new-audit-session/<str:id>', create_new_audit_session, name='create_audit_session'),
     path('audit-categories/<str:id>', get_audit_categories, name="audit category"),
-    path('upload-data', post_audit_data, name="upload data")
+    path('upload-data', post_audit_data, name="upload data"),
+    path('audit-question-session', post_audit_question_session, name="audit-question-session")
 ]
